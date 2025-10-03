eb048fc0cdbd6bde22249771a29456f4b6f55a3a
Dylan Dayrit <157668404+dylndayrit@users.noreply.github.com>
2025-10-03 01:30:45 -0400

    Update Todo input and button styling
    Adjusted margin and color classes for the title and input box. Changed the add button background from orange to rose for improved UI consistency.

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -59,16 +59,16 @@ useEffect(() => {
     <div className = 'bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
 
       {/* ------- title ---------- */}
-      <div className = 'flex items-center mt-7 gap-2'>
+      <div className = 'flex items-center mt-5 gap-2'>
         <img className = 'max-w-10' src={todo_icon}/>
         <h1 className = 'text-3xl font-semibold'>To-Do List</h1>
       </div>
 
       {/* ------- input box -------- */}
-      <div className = 'flex items-center my-7 bg-gray-300 rounded-full'>
+      <div className = 'flex items-center my-5 bg-gray-300 rounded-full'>
         <input ref = {inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' 
         type = "text" placeholder = 'Add your task!' />
-        <button onClick = {add} className= 'border-none outline-none rounded-full bg-orange-800 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+        <button onClick = {add} className= 'border-none outline-none rounded-full bg-rose-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
       </div>
 
       {/* -------- todo list: display all todo items------------ */}
